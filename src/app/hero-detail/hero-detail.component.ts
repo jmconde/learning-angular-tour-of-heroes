0bfae7fc37d44fab55b8a4f7871d464d6ceea3d6
Jose Conde <dev.jose.conde@gmail.com>
2018-09-07 20:22:01 +0200

    first commitº

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,41 @@
+import { Location } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Hero } from '../Hero';
+import { HeroService } from '../hero.service';
+
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit() {
+    this.getHero();
+  }
+
+  getHero() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+}
